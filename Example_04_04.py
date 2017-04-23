178e3361d387b5a2b755b5139b3f11dcefc625cc
Wiid <Andries.Wiid@sasol.com>
2017-04-23 12:45:40 +0200

    PEP8 Changes

--- Example_04_04.py
+++ Example_04_04.py
@@ -46,7 +46,8 @@ print(np.poly1d(G.den, variable='s'))
 
 print('\n1) Eigenvalues are: p1 = ', val[0], 'and p2 = ', val[1])
 print('   with eigenvectors: q1 = ', vec[:, 0], 'and q2 = ', vec[:, 1])
-print('   Input pole vectors are: up1 = ', in_vecs[0], 'and up2 = ', in_vecs[1])
+print('   Input pole vectors are: up1 = ', in_vecs[0],
+      'and up2 = ', in_vecs[1])
 
 print('\n2) The controlabillity matrix has rank', n, 'and is given as:')
 print(c_matrix)
