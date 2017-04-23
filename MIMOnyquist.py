338d5a2f345fabaae7fadebf7b3001d2729f8234
Wiid <Andries.Wiid@sasol.com>
2017-04-23 14:42:29 +0200

    PEP8 Changes

--- MIMOnyquist.py
+++ MIMOnyquist.py
@@ -1,7 +1,7 @@
 """
 Nyquist plot for a MIMO system.
 
-Adapt the already specified transfer funtion matrix to match your system. 
+Adapt the already specified transfer funtion matrix to match your system.
 """
 
 import numpy as np
@@ -14,9 +14,9 @@ K = np.array([[1., 2.],
 t1 = np.array([[5., 5.],
                [5., 5.]])
 t2 = np.array([[5., 6.],
-               [7., 8.]]) 
-#Controller
-Kc = np.array([[0.1, 0.], 
+               [7., 8.]])
+# Controller
+Kc = np.array([[0.1, 0.],
                [0., 0.1]])*6
 
 
@@ -26,6 +26,6 @@ def G(s):
 
 def L(s):
     return Kc*G(s)
-    
+
 mino_nyquist_plot(L, 2, -3, 3)
 plt.show()
