b1c6f131138f82f21ef771b2337656407bc59eac
Wiid <Andries.Wiid@sasol.com>
2017-04-23 14:06:25 +0200

    PEP8 Changes

--- 5_16_2_Room_Heating.py
+++ 5_16_2_Room_Heating.py
@@ -24,11 +24,13 @@ scont.rule_three_four(G, Gd, R=3, freq=freqs, perfect=False)
 
 # From Disturbance Bode you can see |G| > |Gd| always. This implies that
 # there will not be any input constraints wrt disturbances. (Rule 3)
-# I.e. your system will always be strong enough to counteract your disturbances.
+# I.e. your system will always be strong enough to counteract your
+# disturbances.
 
 # Using R = 3 you get an intersection of w_r = 0.0065 which implies that
-# the plant will be able to track set point changes up to w_r. The required response time
-# is 1/1000 = 0.001. Therefore w_r is better than the required response time.
+# the plant will be able to track set point changes up to w_r. The required
+# response time is 1/1000 = 0.001. Therefore w_r is better than the required
+# response time.
 
 plt.figure("Feedback Constraint Bode")
 S = 1/(1 + G*K)
@@ -37,15 +39,15 @@ scont.rule_one_two(S, Gd, R=3, freq=freqs)
 # From Feedback Constraint Bode you can see that the feedback loop of the
 # system will be able to handle any disturbance input.
 
-# However, at about w = 0.004 rad/s you can see that setpoints will not be tracked
-# that is, the error will be more than one.
+# However, at about w = 0.004 rad/s you can see that setpoints will not be
+# tracked that is, the error will be more than one.
 
 plt.figure("Dead Time Constraint on cross over frequency")
 L = G*K
 scont.dead_time_bound(L, Gd, 100, freq=freqs)
 
-# From the output you can see that because the upper bound of w_c is so close to the lower bound of
-# w_c you are going to have a problem.
+# From the output you can see that because the upper bound of w_c is so close
+# to the lower bound of w_c you are going to have a problem.
 
 
 plt.show()
