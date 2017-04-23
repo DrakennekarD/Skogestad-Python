4de1f52ddfc7146d9f6a5736f902d7a8ea2a29bf
Wiid <Andries.Wiid@sasol.com>
2017-04-23 12:41:16 +0200

    PEP8 Changes

--- Example_08_05.py
+++ Example_08_05.py
@@ -4,7 +4,7 @@ import numpy as np
 from robusttools import UnstructuredDelta
 
 
-M = np.matrix([[2., 2.],[-1., -1.]])
+M = np.matrix([[2., 2.], [-1., -1.]])
 
 print('Full perturbation delta matrix')
 print(UnstructuredDelta(M, 'Full'))
