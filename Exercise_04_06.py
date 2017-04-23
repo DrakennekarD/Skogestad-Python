1cba07723bd157a11aa8a606b6c2e7731e90f1f0
Wiid <Andries.Wiid@sasol.com>
2017-04-23 14:56:51 +0200

    PEP8 Changes

--- Exercise_04_06.py
+++ Exercise_04_06.py
@@ -3,9 +3,9 @@ import numpy as np
 
 a11, a12, a21, a22, b21, b22, z = sympy.symbols('a11 a12 a21 a22 b21 b22 z')
 
-A = sympy.Matrix([[a11, a12],[a21, a22]])
-B = sympy.Matrix([[1, 1],[b21, b22]])
-C = sympy.Matrix([[1, 0],[0, 1]])
+A = sympy.Matrix([[a11, a12], [a21, a22]])
+B = sympy.Matrix([[1, 1], [b21, b22]])
+C = sympy.Matrix([[1, 0], [0, 1]])
 D = sympy.Matrix(sympy.Matrix(np.zeros_like(A)))
 
 M = sympy.BlockMatrix([[A, B],
@@ -19,6 +19,6 @@ P = sympy.Matrix(zIg - M)
 zf = P.det()
 print('solve z:', zf)
 zero = sympy.solve(zf, z)
-print("Zero = ",zero)
+print("Zero = ", zero)
 # Result confirmed by para 4.6.7.  C has full rank, and D = zero.  No zero
 # value s = z for which P loses rank
