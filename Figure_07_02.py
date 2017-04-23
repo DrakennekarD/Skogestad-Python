ec2d6b3755af2e519c4741484c79ac54ba043085
Wiid <Andries.Wiid@sasol.com>
2017-04-23 15:10:05 +0200

    PEP8 Changes

--- Figure_07_02.py
+++ Figure_07_02.py
@@ -3,11 +3,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def G_P(k, tau, theta, s):
     return k/(tau*s + 1)*np.exp(-theta*s)
 
+
 def G(s):
     return G_P(2.5, 2.5, 2.5, s)
+
 
 def w_A(s):
     k = 0.5
@@ -16,11 +19,13 @@ def w_A(s):
     tau3 = 1/3.0
     return k*(tau1*s + 1)/(tau2*s + 1)/(tau3*s + 1)
 
+
 def circle(centerx, centery, radius):
     angle = np.linspace(0, 2*np.pi)
     x = centerx + np.cos(angle)*radius
     y = centery + np.sin(angle)*radius
     plt.plot(x, y, 'r-')
+
 
 def randomparameters():
     return [2 + np.random.rand() for i in range(3)]
@@ -59,6 +64,6 @@ for i in range(N):
 plt.loglog(omega, distance, 'b--')
 plt.loglog(omega, list(map(abs, w_A(s))), 'r')
 plt.xlabel('Frequency')
-plt.legend(['Distance','w_A'])
+plt.legend(['Distance', 'w_A'])
 
 plt.show()
