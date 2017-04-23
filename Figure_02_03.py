ec8ca423f9639b45498d7f72f25e494bbbc53f20
Wiid <Andries.Wiid@sasol.com>
2017-04-23 12:48:06 +0200

    PEP8 Changes

--- Figure_02_03.py
+++ Figure_02_03.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from utils import tf
 from utilsplot import bode
 
-s = tf([1,0], 1)
+s = tf([1, 0], 1)
 G = 30*(s + 1)/((s + 10)*(s + 0.01)**2)
 
 plt.figure('Figure 2.3')
